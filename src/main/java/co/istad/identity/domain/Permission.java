b640fc0d96e9659348ee50bedc4a5da10ad4de87
KungSovannda <kungsovannda@gmail.com>
2025-12-11 10:25:12 +0700

    feat: domain user role permission

--- src/main/java/co/istad/identity/domain/Permission.java
+++ src/main/java/co/istad/identity/domain/Permission.java
@@ -1,4 +1,30 @@
 package co.istad.identity.domain;
 
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "permissions")
 public class Permission {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String name;
+
+    @ManyToMany(mappedBy = "permissions")
+    private List<User> users;
+
+    @ManyToMany(mappedBy = "permissions")
+    private List<Role> roles;
 }
--- src/main/java/co/istad/identity/domain/Role.java
+++ src/main/java/co/istad/identity/domain/Role.java
@@ -1,4 +1,36 @@
 package co.istad.identity.domain;
 
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+import java.util.Set;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "roles")
 public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    private List<User> users;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "roles_permissions",
+            joinColumns = @JoinColumn(name = "role_id"),
+            inverseJoinColumns = @JoinColumn(name = "permission_id")
+    )
+    private Set<Permission> permissions;
+
 }
