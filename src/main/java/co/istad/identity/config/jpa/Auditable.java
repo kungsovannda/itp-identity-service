74c60330a25ddaf965231bb7299f7714e9f43f7b
KungSovannda <kungsovannda@gmail.com>
2025-12-11 10:24:36 +0700

    feat: config audit

--- src/main/java/co/istad/identity/config/jpa/Auditable.java
+++ src/main/java/co/istad/identity/config/jpa/Auditable.java
@@ -1,4 +1,34 @@
 package co.istad.identity.config.jpa;
 
-public class Auditable {
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.Instant;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class Auditable<T> {
+
+    @CreatedBy
+    protected T createdBy;
+
+    @CreatedDate
+    protected Instant createdAt;
+
+    @LastModifiedBy
+    protected T lastModifiedBy;
+
+    @LastModifiedDate
+    protected Instant lastModifiedAt;
 }
