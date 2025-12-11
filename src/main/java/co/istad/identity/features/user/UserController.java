3a8955a3a4b42a24908adff82c766a4d7569e63f
KungSovannda <kungsovannda@gmail.com>
2025-12-11 12:01:01 +0700

    feat: implement core service of oauth 2

--- src/main/java/co/istad/identity/features/user/UserController.java
+++ src/main/java/co/istad/identity/features/user/UserController.java
@@ -1,5 +1,6 @@
 package co.istad.identity.features.user;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import java.util.Map;
 public class UserController {
 
     @GetMapping
+    @PreAuthorize("hasAuthority('user:read:own')")
     public Map<?, ?> getUsers() {
         return Map.of("message", "User retrieve successfully");
     }
--- src/main/java/co/istad/identity/security/UserDetailsServiceImpl.java
+++ src/main/java/co/istad/identity/security/UserDetailsServiceImpl.java
@@ -1,18 +1,22 @@
 package co.istad.identity.security;
 
-import co.istad.identity.domain.Role;
 import co.istad.identity.domain.User;
 import co.istad.identity.features.user.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 
-import java.util.stream.Collectors;
+import java.util.ArrayList;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Component
+@Slf4j
 public class UserDetailsServiceImpl implements UserDetailsService {
 
     private final UserRepository userRepository;
@@ -23,12 +27,23 @@ public class UserDetailsServiceImpl implements UserDetailsService {
                 () -> new UsernameNotFoundException("User not found with username: " + username)
         );
 
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        user.getRoles().forEach(role -> {
+            authorities.add(new SimpleGrantedAuthority("ROLE_" + role.getName()));
+            role.getPermissions().forEach(permission -> {
+                authorities.add(new SimpleGrantedAuthority(permission.getName()));
+            });
+        });
+
+        log.info("USER AUTHORITIES: {}", authorities);
+
         return org.springframework.security.core.userdetails.User.withUsername(user.getUsername())
                 .disabled(!user.getIsEnabled())
                 .credentialsExpired(!user.getCredentialsNonExpired())
                 .accountLocked(!user.getAccountNonLocked())
                 .accountExpired(!user.getAccountNonExpired())
-                .roles(user.getRoles().stream().map(Role::getName).toArray(String[]::new))
+//                .roles(user.getRoles().stream().map(Role::getName).toArray(String[]::new))
+                .authorities(authorities)
                 .password(user.getPassword())
                 .build();
     }
